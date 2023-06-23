d504542b9b56bc7c53f73fd08e63a31e2f0b59d6
Poudel0 <poudel0sandesh@gmail.com>
2023-06-23 21:51:27 +0545

    SearchBar done

--- src/component/Profile/Profile.jsx
+++ src/component/Profile/Profile.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "./Profile.css";
 
 const Profile = () => {
-  return <section id="Profile" />;
+  return (
+    <section id="Profile">
+      <div className="Profile_base">Hello</div>
+    </section>
+  );
 };
 
 export default Profile;
--- src/component/Search/Search.jsx
+++ src/component/Search/Search.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import "./Search.css";
+import { RiUserSearchFill } from "react-icons/ri";
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div className="Search_Wrapper">
+      <div className="Search_Bar">
+        <input
+          type="text"
+          id="Bar"
+          placeholder="0x000000000000000000000000000000000000"
+        />
+        <a href="#">
+          <RiUserSearchFill />
+        </a>
+      </div>
+    </div>
+  );
 };
-// import "./Search.css";
 export default Search;
