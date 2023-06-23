d91cee051c112c8faf4933d5aa1f743bd8fabf3c
Poudel0 <poudel0sandesh@gmail.com>
2023-06-23 07:38:44 +0545

    Nothing shown

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Header from "./component/About/About.jsx";
+import Navbar from "./component/Navbar/Navbar.jsx";
+import Search from "./component/Search/Search.jsx";
+import Work from "./component/Work/Work.jsx";
+import Education from "./component/Education/Education.jsx";
+import Experience from "./component/Experience/Experience.jsx";
+import Contact from "./component/Contact/Contact.jsx";
+import About from "./component/About/About.jsx";
 
 const App = () => {
-  return <div> App</div>;
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <Search />
+      <Work />
+      <Education />
+      <Experience />
+      <Contact />
+      <About />
+    </>
+  );
 };
 
 export default App;
--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import "./Header.css";
 const Header = () => {
-  return <div></div>;
+  return (
+    <header>
+      <div className="container header_container">
+        <h5>Hello </h5>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
