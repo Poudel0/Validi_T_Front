25f1869e3e6da06dce1ef37164bfea04978b52b0
Poudel0 <poudel0sandesh@gmail.com>
2023-06-24 11:36:55 +0545

    Cards skeleton

--- src/component/Profile/Profile.jsx
+++ src/component/Profile/Profile.jsx
@@ -1,10 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Profile.css";
+import Education from "./Education/Education.jsx";
+import Experience from "./Experience/Experience.jsx";
+import Work from "./Work/Work.jsx";
 
 const Profile = () => {
+  const [profileDetails, setProfileDetails] = useState("");
+  const [work, setWork] = useState([]);
+  const [education, setEdiucation] = useState([]);
+  const [experience, setExperience] = useState([]);
+
+  const updateProfileDetails = (details) => {
+    setProfileDetails(details);
+  };
+  const updateWorkDetails = (data) => {
+    setProfileDetails(data);
+  };
+  const updateEducationDetails = (data) => {
+    setProfileDetails(data);
+  };
+  const updateExperienceDetails = (data) => {
+    setProfileDetails(data);
+  };
+
   return (
     <section id="Profile">
-      <div className="Profile_base">Hello</div>
+      <div className="Card">
+        <div className="Card_Header">Your name</div>
+        <div className="Card_Body">
+          <input
+            type="text"
+            value={profileDetails}
+            onChange={(e) => updateProfileDetails(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="Card">
+        <div className="Card_Header">Work History</div>
+        <div className="Card_Body">
+          <input
+            type="text"
+            value={profileDetails}
+            onChange={(e) => updateProfileDetails(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="Card">
+        <div className="Card_Header">Education History</div>
+        <div className="Card_Body">
+          <input
+            type="text"
+            value={profileDetails}
+            onChange={(e) => updateProfileDetails(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="Card">
+        <div className="Card_Header">Experience History</div>
+        <div className="Card_Body">
+          <input
+            type="text"
+            value={profileDetails}
+            onChange={(e) => updateProfileDetails(e.target.value)}
+          />
+        </div>
+      </div>
     </section>
   );
 };
